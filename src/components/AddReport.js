fecb5871cc0554d6ebbf0ad78ef1283ade1a2756
Phillip Pham <masterphillip17@gmail.com>
2022-10-01 02:07:14 -0400

    Add text to Add Incident Report button

--- src/components/AddReport.js
+++ src/components/AddReport.js
@@ -36,74 +36,74 @@ const AddReport = () => {
   const [open, setOpen] = useState(false);
   const [selectedDate, handleDateChange] = useState(new Date());
 
-  return (
-    <>
-      <Tooltip
-        onClick={(e) => setOpen(true)}
-        title="Delete"
-        sx={{
-          position: "fixed",
-          bottom: 20,
-          left: { xs: "calc(50% - 25px)", md: 30 },
-        }}
+  return !open ? (
+    <Tooltip
+      onClick={(e) => setOpen(true)}
+      title="Add"
+      sx={{
+        position: "fixed",
+        bottom: 20,
+        left: { xs: "calc(50% - 20px)", md: 30 },
+      }}
+    >
+      <Fab color="primary" aria-label="add" variant="extended">
+        <AddIcon />
+        <Typography variant="span">Add Incident Report</Typography>
+      </Fab>
+    </Tooltip>
+  ) : (
+    <SytledModal
+      open={open}
+      onClose={(e) => setOpen(false)}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box
+        width={400}
+        height={280}
+        bgcolor={"background.default"}
+        color={"text.primary"}
+        p={3}
+        borderRadius={5}
       >
-        <Fab color="primary" aria-label="add">
-          <AddIcon />
-        </Fab>
-      </Tooltip>
-      <SytledModal
-        open={open}
-        onClose={(e) => setOpen(false)}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box
-          width={400}
-          height={280}
-          bgcolor={"background.default"}
-          color={"text.primary"}
-          p={3}
-          borderRadius={5}
-        >
-          <Typography variant="h6" color="gray" textAlign="center">
-            Create new incident report
+        <Typography variant="h6" color="gray" textAlign="center">
+          Create new incident report
+        </Typography>
+        <UserBox>
+          <Typography fontWeight={600} variant="span">
+            Reporter:
           </Typography>
-          <UserBox>
-            <Typography fontWeight={600} variant="span">
-              Reporter:
-            </Typography>
-            <Avatar sx={{ width: 30, height: 30 }} />
-            <Typography fontWeight={500} variant="span">
-              John Doe
-            </Typography>
-          </UserBox>
-          <TextField
-            sx={{ width: "100%", bottom: 20 }}
-            id="standard-multiline-static"
-            multiline
-            rows={3}
-            placeholder="Enter Description"
-            variant="standard"
+          <Avatar sx={{ width: 30, height: 30 }} />
+          <Typography fontWeight={500} variant="span">
+            John Doe
+          </Typography>
+        </UserBox>
+        <TextField
+          sx={{ width: "100%", bottom: 20 }}
+          id="standard-multiline-static"
+          multiline
+          rows={3}
+          placeholder="Enter Description"
+          variant="standard"
+        />
+        <MuiPickersUtilsProvider utils={DateUtils}>
+          <KeyboardDateTimePicker
+            value={selectedDate}
+            onChange={handleDateChange}
+            label="Date of incident"
+            onError={console.log}
+            format="yyyy/MM/DD hh:mm a"
           />
-          <MuiPickersUtilsProvider utils={DateUtils}>
-            <KeyboardDateTimePicker
-              value={selectedDate}
-              onChange={handleDateChange}
-              label="Date of incident"
-              onError={console.log}
-              format="yyyy/MM/dd hh:mm a"
-            />
-          </MuiPickersUtilsProvider>
-          <ButtonGroup
-            fullWidth
-            variant="contained"
-            aria-label="outlined primary button group"
-          >
-            <Button>Create</Button>
-          </ButtonGroup>
-        </Box>
-      </SytledModal>
-    </>
+        </MuiPickersUtilsProvider>
+        <ButtonGroup
+          fullWidth
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button>Create</Button>
+        </ButtonGroup>
+      </Box>
+    </SytledModal>
   );
 };
 
