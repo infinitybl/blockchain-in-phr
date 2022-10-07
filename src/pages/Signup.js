2599961fa67484846b625d666e03f712c4aa32b7
Phillip Pham <masterphillip17@gmail.com>
2022-10-07 03:37:52 -0400

    Add different user types to Signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
@@ -40,6 +41,7 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function Signup() {
+  const [userType, setUserType] = useState("patient");
   const [phone, setPhone] = useState("");
 
   const handlePhoneChange = (newPhone) => {
@@ -80,6 +82,29 @@ export default function Signup() {
           <Typography component="h1" variant="h5">
             Sign Up
           </Typography>
+          <Box sx={{ marginTop: 2 }}>
+            <InputLabel variant="standard">Select User Type</InputLabel>
+            <ButtonGroup disableElevation variant="contained" color="primary">
+              <Button
+                color={userType === "patient" ? "secondary" : "primary"}
+                onClick={() => setUserType("patient")}
+              >
+                Patient
+              </Button>
+              <Button
+                color={userType === "medicalCompany" ? "secondary" : "primary"}
+                onClick={() => setUserType("medicalCompany")}
+              >
+                Medical Company
+              </Button>
+              <Button
+                color={userType === "government" ? "secondary" : "primary"}
+                onClick={() => setUserType("government")}
+              >
+                Health Ministry
+              </Button>
+            </ButtonGroup>
+          </Box>
           <Box
             component="form"
             noValidate
@@ -97,95 +122,209 @@ export default function Signup() {
                   autoComplete="metamask-wallet-address"
                 />
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <DatePicker
-                  label="Date of Birth"
-                  value={selectedDate}
-                  onChange={handleDateChange}
-                  renderInput={(params) => <TextField {...params} />}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <MuiTelInput
-                  label="Phone Number"
-                  value={phone}
-                  onChange={handlePhoneChange}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  name="email"
-                  label="Email Address"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="gender"
-                  name="gender"
-                  label="Gender"
-                  autoComplete="gender"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="home-address"
-                  name="homeAddress"
-                  label="Home Address"
-                  autoComplete="home-address"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControl fullWidth>
-                  <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    Blood Type
-                  </InputLabel>
-                  <NativeSelect
-                    defaultValue={"O-"}
-                    inputProps={{
-                      name: "blood-type",
-                      id: "uncontrolled-native",
-                    }}
-                  >
-                    <option value={"O-"}>O-</option>
-                    <option value={"O+"}>O+</option>
-                    <option value={"A-"}>A-</option>
-                    <option value={"A+"}>A+</option>
-                    <option value={"B-"}>B-</option>
-                    <option value={"B+"}>B+</option>
-                    <option value={"AB-"}>AB-</option>
-                    <option value={"AB+"}>AB+</option>
-                  </NativeSelect>
-                </FormControl>
-              </Grid>
+              {userType === "patient" && (
+                <>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      autoComplete="given-name"
+                      name="firstName"
+                      required
+                      fullWidth
+                      id="firstName"
+                      label="First Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={6}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="lastName"
+                      label="Last Name"
+                      name="lastName"
+                      autoComplete="family-name"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <DatePicker
+                      label="Date of Birth"
+                      value={selectedDate}
+                      onChange={handleDateChange}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <MuiTelInput
+                      label="Phone Number"
+                      value={phone}
+                      onChange={handlePhoneChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      name="email"
+                      label="Email Address"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="gender"
+                      name="gender"
+                      label="Gender"
+                      autoComplete="gender"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="home-address"
+                      name="homeAddress"
+                      label="Home Address"
+                      autoComplete="home-address"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <FormControl fullWidth>
+                      <InputLabel
+                        variant="standard"
+                        htmlFor="uncontrolled-native"
+                      >
+                        Blood Type
+                      </InputLabel>
+                      <NativeSelect
+                        defaultValue={"O-"}
+                        inputProps={{
+                          name: "blood-type",
+                          id: "uncontrolled-native",
+                        }}
+                      >
+                        <option value={"O-"}>O-</option>
+                        <option value={"O+"}>O+</option>
+                        <option value={"A-"}>A-</option>
+                        <option value={"A+"}>A+</option>
+                        <option value={"B-"}>B-</option>
+                        <option value={"B+"}>B+</option>
+                        <option value={"AB-"}>AB-</option>
+                        <option value={"AB+"}>AB+</option>
+                      </NativeSelect>
+                    </FormControl>
+                  </Grid>
+                </>
+              )}
+              {userType === "medicalCompany" && (
+                <>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="company-name"
+                      name="companyName"
+                      required
+                      fullWidth
+                      id="companyName"
+                      label="Company Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="company-type"
+                      name="companyType"
+                      required
+                      fullWidth
+                      id="companyType"
+                      label="Company Type"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <MuiTelInput
+                      label="Phone Number"
+                      value={phone}
+                      onChange={handlePhoneChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      name="email"
+                      label="Email Address"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="location-address"
+                      name="locationAddress"
+                      label="Office Location Address"
+                      autoComplete="location-address"
+                    />
+                  </Grid>
+                </>
+              )}
+              {userType === "government" && (
+                <>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="health-ministry-name"
+                      name="healthMinistryName"
+                      required
+                      fullWidth
+                      id="healthMinistryName"
+                      label="Health Ministry Name"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      autoComplete="country"
+                      name="country"
+                      required
+                      fullWidth
+                      id="country"
+                      label="Country"
+                      autoFocus
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <MuiTelInput
+                      label="Phone Number"
+                      value={phone}
+                      onChange={handlePhoneChange}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="email"
+                      name="email"
+                      label="Email Address"
+                      autoComplete="email"
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="location-address"
+                      name="locationAddress"
+                      label="Office Location Address"
+                      autoComplete="location-address"
+                    />
+                  </Grid>
+                </>
+              )}
+
               {/* <Grid item xs={12}>
                 <FormControlLabel
                   control={
